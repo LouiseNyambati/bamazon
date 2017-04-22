f136951b570d793654ff16177bde6d0f865733c1
Louise Nyambati <louisen1993@gmail.com>
2017-04-21 23:30:18 -0500

    finished bamazonManager.js

--- bamazonManager.js
+++ bamazonManager.js
@@ -15,25 +15,93 @@ connection.connect(function(err) {
   if (err) throw err;
   // console.log("connected as id " + connection.threadId);
 });
-// var viewProducts = function(){
-//     connection.query("SELECT * FROM products", function(err,results){
-//       console.log(results);
-    // });
-// }
 
-  inquirer.prompt([
-  {
-    name: "Menu",
-    type: "rawlist",
-    message: "What do you want to do?",
-    choices: ["View Products for Sale","View Low Inventory","Add to Inventory","New Item"]
+var start = function(){
+
+    inquirer.prompt([
+    {
+      name: "Menu",
+      type: "rawlist",
+      message: "What do you want to do?",
+      choices: ["View Products for Sale","View Low Inventory","Add to Inventory","New Item"]
+    }
+  ]).then(function(answer) {
+    // console.log("it worked");
+    if (answer.Menu === "View Products for Sale"){
+    connection.query("SELECT * FROM products", function(err,res){
+      console.log(res);
+    });
+  }if (answer.Menu === "View Low Inventory"){
+    connection.query("SELECT * FROM products WHERE stock_quantity=?", [0], function(err,res){
+
+      console.log("These items need to be stocked");
+      console.log(res);
+    });
+
+
+  }if (answer.Menu === "New Item"){
+    inquirer.prompt([
+      {
+        name: "Product",
+        type: "input",
+        message: "Whats the name of the item you want to enter?"
+      },
+      {
+        name: "Department",
+        type: "input",
+        message: "Whats the name of the department your item is in?"
+      },
+      {
+        name: "Price",
+        type: "input",
+        message: "How much does your item cost?"
+      },
+      {
+        name: "Stock",
+        type: "input",
+        message: "How much stock is there?"
+      }
+
+    ]).then(function(answer){
+      connection.query("INSERT INTO products SET ?", {
+        product_name:answer.Product,
+        department_name: answer.Department,
+        price: answer.Price,
+        stock_quantity: answer.Stock
+      }, function(err){
+        if(err) throw err;
+        console.log ("You added a product!");
+
+      })
+    })
+
+
+  }if (answer.Menu === "Add to Inventory"){
+    inquirer.prompt([
+      {
+        name:"identification",
+        type: "input",
+        message: "What is the id number of the item's stock you want to update?"
+      },
+      {
+        name:"Quantity",
+        type: "input",
+        message: "What's the new quantity for this item?"
+      }
+    ]).then(function(answer){
+        connection.query("UPDATE products SET ? WHERE ?",  [{
+          stock_quantity: answer.quantity
+        },{
+          id: answer.identification
+        }])
+        console.log("Updated!")
+    })
+
+
+
   }
-]).then(function(answer) {
-  // console.log("it worked");
-  if (answer === "View Products for Sale"){
-  connection.query("SELECT * FROM products WHERE stock_quantity > 1", function(err,res){
-    console.log(res);
-  });
-}
 
-});
+  });
+
+}
+  start();
