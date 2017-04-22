88ef9f51461a59b99a659499c9e3f9aa154d0a20
Louise Nyambati <louisen1993@gmail.com>
2017-04-21 22:29:35 -0500

    Made changes

--- bamazonManager.js
+++ bamazonManager.js
@@ -15,3 +15,25 @@ connection.connect(function(err) {
   if (err) throw err;
   // console.log("connected as id " + connection.threadId);
 });
+// var viewProducts = function(){
+//     connection.query("SELECT * FROM products", function(err,results){
+//       console.log(results);
+    // });
+// }
+
+  inquirer.prompt([
+  {
+    name: "Menu",
+    type: "rawlist",
+    message: "What do you want to do?",
+    choices: ["View Products for Sale","View Low Inventory","Add to Inventory","New Item"]
+  }
+]).then(function(answer) {
+  // console.log("it worked");
+  if (answer === "View Products for Sale"){
+  connection.query("SELECT * FROM products WHERE stock_quantity > 1", function(err,res){
+    console.log(res);
+  });
+}
+
+});
