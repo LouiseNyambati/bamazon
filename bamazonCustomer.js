213c757e290af7256d9700358ecf0314c1876e75
Louise Nyambati <louisen1993@gmail.com>
2017-04-21 21:19:11 -0500

    Finished bamazonCustomer.js

---
+++ bamazonCustomer.js
@@ -0,0 +1,48 @@
+// NPM packages
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+// Connect to database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "Louise",
+    password: "Louisemn",
+    database: "Bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  // console.log("connected as id " + connection.threadId);
+});
+
+connection.query("SELECT * FROM products", function(err, res) {
+  for (var i = 0; i < res.length; i++) {
+    console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+  }
+  console.log("-----------------------------------");
+});
+
+
+var runSearch = function() {
+  inquirer.prompt([
+    {
+      name: "Item",
+    type: "input",
+    message: "What is the id number of the item you would like to buy?"
+  },
+  {
+    name: "Quantity",
+    type: "input",
+    message: "How many do you want?"
+  }
+  ]).then(function(answer) {
+    connection.query("SELECT * FROM products WHERE id=?", [answer.Item], function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(answer.Quantity + " | " + res[i].product_name + " | "  + "$"+ (res[i].price * answer.Quantity));
+    }
+  });
+
+  })
+};
+runSearch();
